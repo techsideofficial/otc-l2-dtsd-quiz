c678c9d8dc14c45f8f9d126939bcdc633e4e0cb0
TechSide <wienerwolf77@gmail.com>
2025-07-23 11:02:19 +1200

    Add help menu

--- QuizMaster/Program.cs
+++ QuizMaster/Program.cs
@@ -1,4 +1,6 @@
-﻿using QuizMaster.Utils;
+﻿using System.Diagnostics;
+using System;
+using QuizMaster.Utils;
 
 namespace QuizMaster
 {
@@ -15,27 +17,38 @@ namespace QuizMaster
             Console.WriteLine("--QUIZMASTER--");
             Console.WriteLine("1. Start Quiz");
             Console.WriteLine("2. Quit Application");
+            Console.WriteLine("3. Help");
 
             switch (Console.ReadLine())
             {
                 case "1":
                     // Start the quiz
                     Logging.LogMessage("Starting quiz...");
+                    Console.Clear();
                     Quiz.QuizMain();
                     break;
                 case "2":
                     // Quit the application
+                    Console.Clear();
                     Logging.LogMessage("Quitting...");
                     Environment.Exit(0);
+                    break;
+                case "3":
+                    Console.Clear();
+                    Process.Start(new ProcessStartInfo() { 
+                        FileName = "https://github.com/techsideofficial/otc-l2-dtsd-quiz/wiki", 
+                        UseShellExecute = true 
+                    });
                     break;
                 default:
                     Console.WriteLine("Invalid input. Please try again.");
                     Logging.LogError("ConsoleInputError: Invalid input. Please try again.");
                     // Clear the console for better readability.
                     Console.Clear();
-                    PrintAndListenForOptions();
                     break;
             }
+
+            PrintAndListenForOptions();
         }
     }
 }
